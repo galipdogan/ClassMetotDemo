3470ebb0e790c86b62d1cdd63b1c47b93ef9bfca
Galip <Galip@DESKTOP-HRK9HK1>
2021-03-13 02:05:21 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        
+        public void Ekle(Musteri musteri)
+        {
+
+            Console.WriteLine("Müşteri Eklendi: " + musteri.Ad);
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            
+            Console.WriteLine("Müşteri Silindi: " + musteri.Ad);
+        }
+
+
+    }
+
+
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Galip";
+            musteri1.Soyad = "Doğan";
+            musteri1.DogumTarihi = new DateTime(1998, 1, 1);
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Ahmet";
+            musteri2.Soyad = "Ahmet";
+            musteri2.DogumTarihi = new DateTime(1999, 1, 1);
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ali";
+            musteri3.Soyad = "Ali";
+            musteri3.DogumTarihi = new DateTime(2000, 1, 1);
+
+            Musteri[] musteriler = new Musteri[]{ musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri1);
+         
+
+            musteriManager.Ekle(musteri2);
+            musteriManager.Sil(musteri2);
+    
+
+            musteriManager.Ekle(musteri3);
+            musteriManager.Sil(musteri3);
+
+            musteriManager.MusteriListele(musteriler);
+
+            
+
+
+        }
+
+        
+    }
+}
